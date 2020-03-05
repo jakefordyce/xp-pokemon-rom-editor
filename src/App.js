927dd7936a50df8b40b325e4d777e689b6b620f7
Jake Fordyce <JakeFordyce78@gmail.com>
2020-03-05 10:31:52 -0500

    change tabs to use hidden

--- src/App.js
+++ src/App.js
@@ -44,25 +44,25 @@ function App() {
           <MenuButtons />
         </nav>
         <nav className="nav nav-tabs bg-light navbar-expand-lg">
-          <button id="pokemon-tab" className={tabClassNames(0)} onClick={(e) => handleTabClicked(e, 0)}>Pokemon</button>
-          <button id="moves-tab" className={tabClassNames(1)} onClick={(e) => handleTabClicked(e, 1)}>Moves</button>
-          <button id="items-tab" className={tabClassNames(2)} onClick={(e) => handleTabClicked(e, 2)}>Items</button>
-          <button id="types-tab" className={tabClassNames(3)} onClick={(e) => handleTabClicked(e, 3)}>Types</button>
-          <button id="encounters-tab" className={tabClassNames(4)} onClick={(e) => handleTabClicked(e, 4)}>Encounters</button>
-          <button id="trainers-tab" className={tabClassNames(5)} onClick={(e) => handleTabClicked(e, 5)}>Trainers</button>
-          <button id="shops-tab" className={tabClassNames(6)} onClick={(e) => handleTabClicked(e, 6)}>Shops</button>
-          <button id="misc-tab" className={tabClassNames(7)} onClick={(e) => handleTabClicked(e, 7)}>Misc</button>
+          <button className={tabClassNames(0)} onClick={(e) => handleTabClicked(e, 0)}>Pokemon</button>
+          <button className={tabClassNames(1)} onClick={(e) => handleTabClicked(e, 1)}>Moves</button>
+          <button className={tabClassNames(2)} onClick={(e) => handleTabClicked(e, 2)}>Items</button>
+          <button className={tabClassNames(3)} onClick={(e) => handleTabClicked(e, 3)}>Types</button>
+          <button className={tabClassNames(4)} onClick={(e) => handleTabClicked(e, 4)}>Encounters</button>
+          <button className={tabClassNames(5)} onClick={(e) => handleTabClicked(e, 5)}>Trainers</button>
+          <button className={tabClassNames(6)} onClick={(e) => handleTabClicked(e, 6)}>Shops</button>
+          <button className={tabClassNames(7)} onClick={(e) => handleTabClicked(e, 7)}>Misc</button>
         </nav>
       </div>      
       <div >
-        {tab === 0 && <PokemonTab />}
-        {tab === 1 && <MovesTab />}
-        {tab === 2 && <ItemsTab />}
-        {tab === 3 && <TypesTab />}
-        {tab === 4 && <EncountersTab />}
-        {tab === 5 && <TrainersTab />}
-        {tab === 6 && <ShopsTab />}
-        {tab === 7 && <MiscTab />}
+        <div hidden={tab !== 0}><PokemonTab /></div>
+        <div hidden={tab !== 1}><MovesTab /></div>
+        <div hidden={tab !== 2}><ItemsTab /></div>
+        <div hidden={tab !== 3}><TypesTab /></div>
+        <div hidden={tab !== 4}><EncountersTab /></div>
+        <div hidden={tab !== 5}><TrainersTab /></div>
+        <div hidden={tab !== 6}><ShopsTab /></div>
+        <div hidden={tab !== 7}><MiscTab /></div>
       </div>
     </div>
   );
