ae9f5f97ec661f5c695a2f4c42e20cdf3a3e8290
Jake Fordyce <jfordyce@covermymeds.com>
2021-03-08 11:01:17 -0500

    add column to help balance types

--- src/components/TypesTab.js
+++ src/components/TypesTab.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {useStoreState, useStoreActions} from 'easy-peasy';
 
 function TypesTab(){
-    
+
   const dataLoaded = useStoreState(state => state.dataLoaded);
   const types = useStoreState(state => state.pokemonTypes);
   const typeMatchups = useStoreState(state => state.typeMatchups);
@@ -13,23 +13,24 @@ function TypesTab(){
   const addTypeMatchup = useStoreActions(actions => actions.addTypeMatchup);
   const removeTypeMatchup = useStoreActions(actions => actions.removeTypeMatchup);
   const generation = useStoreState(state => state.romModelState.generation);
-  
+
   const usedTypes = types.filter((type) => type.typeIsUsed);
-  
-  const typeList = types.map((type, index) => 
+
+  const typeList = types.map((type, index) =>
     <tr key={index}>
       <td><input style={{width: "100px"}} value={type.typeName} onChange={(e) => handleTypeChange(e, index, 'typeName')} /></td>
       <td><input type="checkbox" checked={type.typeIsUsed} onChange={(e) => handleTypeCheckbox(e, index, 'typeIsUsed')} /></td>
       <td><button onClick={(e) => handleRemoveType(e, index)}>X</button></td>
-      {(index < 20 && generation < 3) && <td>Att</td>}      
+      {(index < 20 && generation < 3) && <td>Att</td>}
       {(index >= 20 && generation === 1) && <td>Spec</td>}
       {(index >= 20 && generation === 2) && <td>Sp. Att</td>}
       {(index < 10 && generation === 3) && <td>Att</td>}
       {(index >= 10 && generation === 3) && <td>Sp. Att</td>}
+      <td>{typeStrengthBalance(type)}</td>
     </tr>
   );
 
-  const typeChartHeaders = usedTypes.map((type, index) => 
+  const typeChartHeaders = usedTypes.map((type, index) =>
     <th className="type-header" key={index}><div><span>{type.typeName}</span></div></th>
   );
 
@@ -56,12 +57,30 @@ function TypesTab(){
         }
       }
 
-      return <td className={effectiveCSS} key={defIndex} onClick={(e) => handleTypeMatchupChange(e, type, defType)} 
+      return <td className={effectiveCSS} key={defIndex} onClick={(e) => handleTypeMatchupChange(e, type, defType)}
       onContextMenu={(e) => {e.preventDefault(); handleTypeMatchupChange(e, type, defType)}} >{cellText}</td>
     });
     return <tr key={index}><td>{type.typeName}</td>{cells}</tr>
   });
-  
+
+  function typeStrengthBalance(type){
+    let balance = 0;
+    typeMatchups.forEach((m) => {
+      if((m.attackType === type.typeIndex && m.effectiveness === 20) ||
+      (m.defenseType === type.typeIndex && m.effectiveness === 5) ||
+      (m.defenseType === type.typeIndex && m.effectiveness === 0)
+      ){
+        balance++;
+      }else if((m.defenseType === type.typeIndex && m.effectiveness === 20) ||
+      (m.attackType === type.typeIndex && m.effectiveness === 5) ||
+      (m.attackType === type.typeIndex && m.effectiveness === 0)
+      ){
+        balance--;
+      }
+    });
+    return balance;
+  }
+
   function handleTypeCheckbox(event, typeIndex, propName){
     let newValue = event.target.checked;
     updateType({index: typeIndex, propName: propName, propValue: newValue});
@@ -74,7 +93,7 @@ function TypesTab(){
 
   function handleTypeMatchupChange(event, attType, defType){
     let matchup = typeMatchups.find((match) => match.defenseType === defType.typeIndex && match.attackType === attType.typeIndex);
-    
+
     if(event.button === 0){ //left click cycle through damage modifiers
       if(matchup !== undefined){
         if(matchup.effectiveness === 0){
@@ -113,6 +132,7 @@ function TypesTab(){
               <th>Is Used</th>
               <th></th>
               <th>Stat</th>
+              <th>Balance</th>
             </tr>
           </thead>
           <tbody>
